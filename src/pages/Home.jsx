360625f3e5397562b8b53d724dc84fc9b244c480
Sachin kumar gupta <sachinkg1309@gmail.com>
2024-05-22 21:41:49 +0530

    updated features and UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,68 +1,96 @@
 import React, { useEffect, useState } from "react";
 import authService from "../appwrite/config";
+import auth from "../appwrite/auth";
 import { Container, PostCard } from "../components";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-
 function Home() {
-  const [posts, setPosts] = useState([]);
-  const Login = useSelector((state) => state.auth.status);
+	const [posts, setPosts] = useState([]);
+	const [toView, setToView] = useState(false);
+	const Login = useSelector((state) => state.auth.status);
 
-  useEffect(() => {
-    authService.getPosts().then((post) => {
-      if (post) {
-        setPosts(post.documents);
-      }
-    });
-  }, []);
+	useEffect(() => {
+		authService.getPosts().then((post) => {
+			if (post) {
+				setPosts(post.documents);
+			}
+		});
+	}, []);
 
-  // if (posts.length === 0) {
-  //   return (
-  //     <div className="w-full py-8 mt-4 text-center">
-  //       <Container>
-  //         <div className="flex flex-wrap">
-  //           <div className="p-2 w-full">
-  //             {Login ? null : (
-  //               <h1 className="text-2xl font-bold hover:text-gray-500">
-  //                 Login to see posts
-  //               </h1>
-  //             )}
-  //           </div>
-  //         </div>
-  //       </Container>
-  //     </div>
-  //   );
-  // }
-  return (
-    <>
-      {Login ? (
-        <div className="w-full py-8 sm:h-[calc(100vh-85px)] pb-10">
-          <Container>
-            <div className="sm:flex flex-wrap">
-              {posts.map((post) => (
-                <div key={post.$id} className="p-2 w-full sm:w-1/2 md:w-1/4">
-                  <PostCard {...post} /* post={post} */ />
-                </div>
-              ))}
-            </div>
-          </Container>
-        </div>
-      ) : (
-          <Container>
-            <div className="w-full py-8 mt-4 text-center h-[calc(100vh-95px)]">
-                <div className="p-2 w-full flex justify-center align-center">
-                  {Login ? null : (
-                    <h1 className="text-2xl text-gray-200 font-bold">
-                      <Link to="/login" className=" underline underline-offset-2 text-gray-300">Login</Link> to see posts.
-                    </h1>
-                  )}
-                </div>
-            </div>
-          </Container>
-      )}
-    </>
-  );
+	const toogleDisplay = () => {
+		setToView((prev) => !prev);
+		setTimeout(() => {
+			setToView(false);
+		}, 2000);
+	};
+	// const user = auth.getCurrentUser();
+
+	// if (posts.length === 0) {
+	//   return (
+	//     <div className="w-full py-8 mt-4 text-center">
+	//       <Container>
+	//         <div className="flex flex-wrap">
+	//           <div className="p-2 w-full">
+	//             {Login ? null : (
+	//               <h1 className="text-2xl font-bold hover:text-gray-500">
+	//                 Login to see posts
+	//               </h1>
+	//             )}
+	//           </div>
+	//         </div>
+	//       </Container>
+	//     </div>
+	//   );
+	// }
+	return (
+		<>
+			{Login ? (
+				<div className="w-[90vw] mx-auto py-8 pb-10">
+					<>
+						<div className="sm:flex flex-wrap">
+							{posts.map((post) => (
+								<div
+									key={post.$id}
+									className="p-2 min-h-[345px] w-full sm:w-1/2 lg:w-1/3 xl:w-1/4">
+									<PostCard {...post} /* post={post} */ />
+									{/* {console.log("User: " + user)} */}
+								</div>
+							))}
+						</div>
+					</>
+				</div>
+			) : (
+				<Container>
+					<div className="w-full flex justify-center items-center py-8 mt-4 text-center min-h-[calc(100vh-200px)]">
+						<div className="p-2">
+							{Login ? null : (
+								<>
+									<h1 className="text-3xl text-gray-200 font-bold">
+										<Link
+											to="/login"
+											className=" underline underline-offset-2 text-gray-300">
+											Login
+										</Link>{" "}
+										to see posts.
+									</h1>
+									<p
+										onClick={toogleDisplay}
+										className="text-gray-500 text-xs cursor-pointer">
+										Skip signup ?<br />
+										<span className={`${toView ? "block" : "hidden"}`}>
+											email: test@gmail.com || password: test1234
+										</span>
+										<br />
+									</p>
+								</>
+							)}
+						</div>
+					</div>
+				</Container>
+			)}
+		</>
+	);
 }
 
 export default Home;
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,65 +1,149 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import appwriteService from "../appwrite/config";
+import appwriteAuth from "../appwrite/auth";
 import { Button, Container } from "../components";
 import parse from "html-react-parser";
 import { useSelector } from "react-redux";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { GiSpeaker } from "react-icons/gi";
+import { FaRegCircleStop } from "react-icons/fa6";
 
 export default function Post() {
-  const [post, setPost] = useState(null);
-  const { slug } = useParams();
-  const navigate = useNavigate();
+	const [post, setPost] = useState(null);
+	const [user, setUser] = useState(null);
+	const [isSpeaking, setIsSpeaking] = useState(true);
+	const { slug } = useParams();
+	const navigate = useNavigate();
+	const userData = useSelector((state) => state.auth.userData);
 
-  const userData = useSelector((state) => state.auth.userData);
+	const isAuthor = post && userData ? post.userID === userData.$id : false;
 
-  const isAuthor = post && userData ? post.userID === userData.$id : false;
+	useEffect(() => {
+		if (slug) {
+			appwriteService.getPost(slug).then((post) => {
+				if (post) setPost(post);
+				else navigate("/");
+			});
+		} else navigate("/");
+	}, [slug, navigate]);
 
-  useEffect(() => {
-    if (slug) {
-      appwriteService.getPost(slug).then((post) => {
-        if (post) setPost(post);
-        else navigate("/");
-      });
-    } else navigate("/");
-  }, [slug, navigate]);
+	const deletePost = () => {
+		appwriteService.deletePost(post.$id).then((status) => {
+			if (status) {
+				appwriteService.deleteFile(post.featuredImage);
+				navigate("/");
+			}
+		});
+	};
+	if (!post) {
+		return (
+			<div className="bg-black/10 w-full h-screen text-6xl text-gray-200">
+				Loading...
+			</div>
+		);
+	}
+	const createdAt = new Date(post.$createdAt);
+	const updatedAt = new Date(post.$updatedAt);
 
-  const deletePost = () => {
-    appwriteService.deletePost(post.$id).then((status) => {
-      if (status) {
-        appwriteService.deleteFile(post.featuredImage);
-        navigate("/");
-      }
-    });
-  };
+	if (isNaN(updatedAt)) {
+		return <div>Invalid date</div>;
+	}
 
-  return post ? (
-    <div className="py-8">
-      <Container>
-        <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
-          <img
-            src={appwriteService.getFilePreview(post.featuredImage)}
-            alt={post.title}
-            className="rounded-xl"
-          />
+	// Extract date and time
+	const dateOptions = { year: "numeric", month: "long", day: "numeric" };
+	const formattedDate = updatedAt.toLocaleDateString(undefined, dateOptions);
 
-          {isAuthor && (
-            <div className="absolute right-6 top-6">
-              <Link to={`/edit-post/${post.$id}`}>
-                <Button bgColor="bg-green-500" className="mr-3">
-                  Edit
-                </Button>
-              </Link>
-              <Button bgColor="bg-red-500" onClick={deletePost}>
-                Delete
-              </Button>
-            </div>
-          )}
-        </div>
-        <div className="w-full mb-6">
-          <h1 className="text-2xl font-bold">{post.title}</h1>
-        </div>
-        <div className="browser-css">{parse(post.content)}</div>
-      </Container>
-    </div>
-  ) : null;
+	const timeOptions = {
+		hour: "2-digit",
+		minute: "2-digit",
+	};
+	const formattedTime = updatedAt.toLocaleTimeString(undefined, timeOptions);
+
+	const toggleSpeak = ({ content }) => {
+		if (isSpeaking) {
+			setIsSpeaking(false);
+			// const text = document.getElementById(`${}`);
+			const utterance = new SpeechSynthesisUtterance(
+				content || "Empty Text area."
+			);
+			console.log("Clicked " + content);
+
+			// Set the onend handler on the same utterance instance
+			utterance.onend = () => {
+				setIsSpeaking(true);
+			};
+
+			speechSynthesis.speak(utterance);
+		} else {
+			speechSynthesis.cancel();
+			setIsSpeaking(true);
+		}
+	};
+
+	return post ? (
+		<Container>
+			<div className="relative py-16 sm:py-8 text-gray-50 min-h-[calc(100vh-80px)]">
+				<div className=" ">
+					<div className="w-full  mb-4   rounded-xl">
+						<img
+							src={appwriteService.getFilePreview(post.featuredImage)}
+							alt={post.title}
+							className="rounded-xl border shadow-md shadow-gray-800"
+						/>
+
+						{isAuthor && (
+							<div className="absolute right-1 top-2 ">
+								<Link to={`/edit-post/${post.$id}`}>
+									<Button bgColor="bg-green-500" className="mr-3 group">
+										<div className="absolute bottom-10 left-1/4 text-xs font-light transform -translate-x-1/2 bg-green-300/50 text-gray-200 px-2 py-1 rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity before:content-[''] before:absolute before:top-full before:left-1/2 before:-translate-x-1/2 before:border-8 before:border-transparent before:border-t-green-300/50">
+											Edit
+										</div>
+										<FaEdit className="text-black" />
+									</Button>
+								</Link>
+								<Button
+									bgColor="bg-red-500  "
+									className="group"
+									onClick={deletePost}>
+									<div className="absolute bottom-10 -right-1/3 text-xs font-light transform -translate-x-1/2 bg-red-300/50 text-gray-200 px-2 py-1 rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity before:content-[''] before:absolute before:top-full before:left-1/2 before:-translate-x-1/2 before:border-8 before:border-transparent before:border-t-red-300/50">
+										Delete
+									</div>
+									<MdDelete />
+								</Button>
+							</div>
+						)}
+					</div>
+				</div>
+				<div className="w-full mb-6 flex items-center">
+					<h1 className="text-3xl text-left font-bold" id="textArea">
+						{post.titles}
+					</h1>
+					<button
+						className="text-white text-xl px-2 ml-3 pt-1"
+						onClick={() => toggleSpeak(post)}>
+						{isSpeaking ? (
+							<GiSpeaker className="border -1" />
+						) : (
+							<FaRegCircleStop />
+						)}
+					</button>
+				</div>
+				<div className="browser-css text-left">{parse(post.content)}</div>
+
+				<div className="w-full text-right text-xs text-gray-400 mt-6">
+					{/* <span className="block">{"Created by: "+appwriteAuth.getUserByUserID()}</span> */}
+					<span
+						className={`${
+							post.$createdAt === post.$updatedAt ? "hidden" : "inline-block"
+						}`}>
+						{"Edited: "}
+					</span>
+					{" " + formattedDate + "  " + formattedTime}
+				</div>
+				{/* {console.log(post)} */}
+			</div>
+		</Container>
+	) : null;
 }
